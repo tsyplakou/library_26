1a7da32d1943e73cd486ef245eb59ca68a338eb2
danil_proskurin <danielpashtetov@gmail.com>
2024-11-15 01:05:39 +0700

    add add_genre, edit_genre views and separate pages

--- genre/views.py
+++ genre/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Genre
 
 
@@ -12,3 +12,26 @@ def genre(request, genre_id):
     if request.method == 'GET':
         genre = Genre.objects.get(id=genre_id)
         return render(request, 'genre.html', {'genre': genre})
+
+
+def add_genre(request):
+    if request.method == 'POST':
+        Genre.objects.create(
+            name=request.POST['name'],
+            description=request.POST['description']
+        )
+        return redirect('genres')
+
+    return render(request, 'add_genre.html')
+
+
+def edit_genre(request, genre_id):
+    genre = Genre.objects.get(id=genre_id)
+
+    if request.method == "POST":
+        genre.name = request.POST['name']
+        genre.description = request.POST['description']
+        genre.save()
+        return redirect('genre', genre_id=genre_id)
+
+    return render(request, 'edit_genre.html', {'genre': genre})
