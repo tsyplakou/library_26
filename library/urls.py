7124ce1fa9f9113fdb096bd8bb37845c256a3813
Andrei Burvel <burvelandrei@gmail.com>
2024-11-14 21:08:56 +0300

    add include book

--- library/urls.py
+++ library/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from user.views import login_view, logout_view, register_view
 
@@ -25,4 +25,5 @@ urlpatterns = [
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
     path('registration/', register_view, name='registration'),
+    path('', include('book.urls'))
 ]
