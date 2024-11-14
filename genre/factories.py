0472a3e43d9276a710068e1309915074ee5f0ea7
danil_proskurin <danielpashtetov@gmail.com>
2024-11-15 00:10:57 +0700

    1. add Genre admin page with filter and search fields 2. Add factory to generate Genres

---
+++ genre/factories.py
@@ -0,0 +1,11 @@
+import factory
+from .models import Genre
+from library.test_data import genres_data
+
+
+class GenreFactory(factory.DjangoModelFactory):
+	class Meta:
+		model = Genre
+
+	name = factory.fuzzy.FuzzyChoice(genre['name'] for genre in genres_data)
+	description = factory.fuzzy.FuzzyChoice(genre['description'] for genre in genres_data)
--- library/settings.py
+++ library/settings.py
@@ -85,12 +85,8 @@ WSGI_APPLICATION = 'library.wsgi.application'
 
 DATABASES = {
     'default': {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ['DB_NAME'],
-        "USER": os.environ['DB_USER'],
-        "PASSWORD": os.environ['DB_PASSWORD'],
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
