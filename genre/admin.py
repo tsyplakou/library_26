14f3985246c0a44f037da5e3751380a50ff7dc8d
danil_proskurin <danielpashtetov@gmail.com>
2024-11-15 00:41:42 +0700

    change tabs to spaces, get settings back

--- genre/admin.py
+++ genre/admin.py
@@ -4,7 +4,7 @@ from .models import Genre
 
 @admin.register(Genre)
 class GenreAdmin(admin.ModelAdmin):
-	list_display = ('name', 'description')
-	search_fields = ('name', 'description')
-	list_filter = ('name', 'description')
+    list_display = ('name', 'description')
+    search_fields = ('name', 'description')
+    list_filter = ('name', 'description')
 
