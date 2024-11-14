c284dcea7fcd5db2afa6b0644f3fc3887eba0dfd
Andrei Burvel <burvelandrei@gmail.com>
2024-11-14 20:52:20 +0300

    views book

--- book/views.py
+++ book/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Book
 
-# Create your views here.
+def index(request):
+    books = Book.objects.all()
+    return render(request, "book/book_shelf.html", {"books": books})
+
+
+def book(request, id):
+    book = get_object_or_404(Book, id=id)
+    return render(request, "book/book.html", {"book": book})
